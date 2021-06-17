d8413322acd102dd469a2d2fdb2e8d595c8716e7
Max Elkins <max@maxelkins.me>
2021-06-17 18:17:47 +0100

    Fixed modifiers

--- keyboards/sofle/keymaps/MaxElkins/keymap.c
+++ keyboards/sofle/keymaps/MaxElkins/keymap.c
@@ -44,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_GRAVE, \
  C(S(KC_LALT)),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
         KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, _______,     _______,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                        KC_LGUI,KC_LALT,KC_LCTRL, KC_LOWER, KC_BSPC,    KC_SPC,  KC_RAISE, KC_LBRC, KC_RBRC, KC_EQL \
-),
+                        KC_LCTRL,KC_LGUI,KC_LALT, KC_LOWER, KC_BSPC,    KC_SPC,  KC_RAISE, KC_LBRC, KC_RBRC, KC_EQL \
+)
 
 /* LOWER
  * ,----------------------------------------.                    ,-----------------------------------------.
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______,   KC_UP, _______, _______, _______,                           _______, _______, _______, _______, _______, _______, \
   KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,                           _______, _______, _______, _______, _______, _______, \
   KC_LSFT, _______, _______, _______, _______, _______,  _______,        _______, _______, _______, _______, _______, KC_BSLS, KC_RSFT, \
-                    KC_LGUI,KC_LALT,KC_LCTRL, KC_LOWER, _______,        _______, KC_RAISE, _______, _______, KC_MINS \
+                    KC_LCTRL,KC_LGUI,KC_LALT, KC_LOWER, _______,        _______, KC_RAISE, _______, _______, KC_MINS \
 ),
 
 /* RAISE
@@ -89,29 +89,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, _______, KC_TILD, \
   _______, _______, _______, _______, _______, _______,                          _______, _______, _______, _______, KC_COLN, KC_DQUO, \
   KC_LSFT, _______, _______, _______, _______, _______, _______,       _______,  _______, _______, KC_LABK, KC_RABK, KC_QUES, KC_RSFT, \
-                    KC_LGUI,KC_LALT,KC_LCTRL, KC_LOWER, _______,       _______, KC_RAISE, KC_LCBR, KC_RCBR, KC_PLUS \
+                    KC_LCTRL,KC_LGUI,KC_LALT, KC_LOWER, _______,       _______, KC_RAISE, KC_LCBR, KC_RCBR, KC_PLUS \
 ),
 
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |MACWIN|      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |                    |VOLDO |VOLUP | MUTE |      |      | PLAY |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | RESET|      |QWERTY|      |      |      |                    |      |      |      |      |      |      |
+ * |       |     |      |      |      |      |                    |      |      |      |      |      |KC_F13|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------|      | VOLDO| MUTE | VOLUP|      |      |
- * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|      | PREV | PLAY | NEXT |      |      |
+ * |      |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
+ * |------+------+------+------+------+------|         |  |       |------+------+------+------+------+------|
+ * |      |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
+ *            |      |      |      |LOWER | /       /       \      \  |RAISE |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
   [_ADJUST] = LAYOUT( \
-  CG_TOGG , XXXXXXX,  XXXXXXX ,  XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  RESET  , XXXXXXX,KC_QWERTY,XXXXXXX,CG_TOGG,XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-  XXXXXXX , XXXXXXX,XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX,                     XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, \
-  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX, \
-                   _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______ \
+  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_VOLD, KC_VOLU, KC_MUTE, XXXXXXX, XXXXXXX, KC_MPLY, \
+  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_F13, \
+  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+  XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+                   _______, _______, _______, KC_LOWER, _______,     _______, KC_RAISE, _______, _______, _______ \
   )
 };
 
