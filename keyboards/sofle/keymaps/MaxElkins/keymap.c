de0b7f73ac78540865923fc50c6db496b923b6cd
Max Elkins <max@maxelkins.me>
2021-06-20 00:15:33 +0100

    Changed space,enter,bk + updated rules bootloader

--- keyboards/sofle/keymaps/MaxElkins/keymap.c
+++ keyboards/sofle/keymaps/MaxElkins/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |  ESC |  1 ! |  2 @ |  3 # |  4 $ |  5 % |                    |  6 ^ |  7 & |  8 * |  9 ( |  0 ) | Enter|
+ * |  ESC |  1 ! |  2 @ |  3 # |  4 $ |  5 % |                    |  6 ^ |  7 & |  8 * |  9 ( |  0 ) | Bkspc|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  `~  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -34,17 +34,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |  ,<  |  .>  |  /?  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCTR | LGUI | LAlt |LOWER | /Bkspc  /       \Space \  |RAISE |  [{  |  ]}  |  =+  |
+ *            | LCTR | LGUI | LAlt |LOWER | /Space  /       \Enter \  |RAISE |  [{  |  ]}  |  =+  |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 
 [_QWERTY] = LAYOUT( \
-        KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_ENT, \
+        KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC, \
         KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_GRAVE, \
  C(S(KC_LALT)),   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
         KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, _______,     _______,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                        KC_LCTRL,KC_LGUI,KC_LALT, KC_LOWER, KC_BSPC,    KC_SPC,  KC_RAISE, KC_LBRC, KC_RBRC, KC_EQL \
+                        KC_LCTRL,KC_LGUI,KC_LALT, KC_LOWER, KC_SPC,    KC_ENT,  KC_RAISE, KC_LBRC, KC_RBRC, KC_EQL \
 ),
 
 /* LOWER
